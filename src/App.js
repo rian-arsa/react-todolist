b22e45ab203a32ef14a55eb53feb0987f1d03f35
Rian Arsa <55070460+rian-arsa@users.noreply.github.com>
2020-10-22 18:33:59 +0700

    Migrasi html to React

--- src/App.js
+++ src/App.js
@@ -3,9 +3,39 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello Rian</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="frame">
+        <section className="container-header">
+          <button className="header-btn">Add</button>
+          <h1 className="header-title">Todo Lists</h1>
+          <button className="header-btn clear">Clear</button>
+        </section>
+
+        <section className="container-add">
+          <form action="" className="add-form">
+            <input type="text" name="todo" id="add-input" />
+            <button className="add-btn">Add</button>
+          </form>
+        </section>
+
+        <section className="container-todos">
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
