1637c8984d8958c0dc0ca752e8147e420b301c3f
Rian Arsa <55070460+rian-arsa@users.noreply.github.com>
2020-10-26 22:35:43 +0700

    Menambahkan placeholder text

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -6,17 +6,25 @@ import Todo from "./Todo";
 const Todos = ({ todos, CompleteTodo }) => {
   return (
     <section className="container-todos">
-      {todos.map((todo, index) => {
-        return (
-          <Todo
-            key={index}
-            text={todo.text}
-            isCompleted={todo.isCompleted}
-            CompleteTodo={CompleteTodo}
-            index={index}
-          />
-        );
-      })}
+      {todos.length > 0 &&
+        todos.map((todo, index) => {
+          return (
+            <Todo
+              key={index}
+              text={todo.text}
+              isCompleted={todo.isCompleted}
+              CompleteTodo={CompleteTodo}
+              index={index}
+            />
+          );
+        })}
+      {todos.length === 0 && (
+        <div className="todo-placeholder-text">
+          Add to by clicking{" "}
+          <span className="add-button-placeholder-text"> Add</span> button onthe
+          top left
+        </div>
+      )}
     </section>
   );
 };
